b21cfd0e697f4bb15dac15ad9a80837a2131b627
LrySpace <2737510299@qq.com>
2026-07-08 23:06:45 +0800

    修改导航栏配置

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -79,13 +79,13 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		children: [
 			{
 				name: "GitHub",
-				url: "https://github.com/CuteLeaf/Firefly",
+				url: "https://github.com/LrySpace/Firefly",
 				external: true,
 				icon: "fa7-brands:github",
 			},
 			{
 				name: "Gitee",
-				url: "https://gitee.com/CuteLeaf/Firefly",
+				url: "https://gitee.com/LrySpace/Firefly",
 				external: true,
 				icon: "fa7-brands:gitee",
 			},
